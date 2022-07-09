5bf709ae40db2aeec14f8fed38b0f5cfd3c149f4
Dominik13910 <dominik.czesnoic@o2.pl>
2022-07-09 13:44:06 +0200

    Weather

--- assets/js/ap.js
+++ assets/js/ap.js
@@ -1,15 +1,52 @@
 let weatherForm = document.querySelector(".weather__form")
 let weatherCity = document.querySelector(".weather__city")
 let weatherApi = document.querySelector(".weather__api")
-let APIURL = "http://api.weatherapi.com/v1/current.json?key=fe528a160ff44491b6f74050220907&aqi=no&q="
+let APIURL = "http://api.weatherapi.com/v1/forecast.json?key=fe528a160ff44491b6f74050220907&aqi=no&days=4&q="
 
 weatherForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let city = weatherCity.value;
-    if (city == ""){
-        weatherCity.style.border ="2px solid red";
-        return false;
+    event.preventDefault()
+    let city = weatherCity.value
+    if (city == "") {
+        weatherCity.style.border = "2px solid red"
+        return false
     }
-    weatherCity.style.border ="none";
+    weatherCity.style.border = "none"
+    let fullAPIURL = APIURL + city
+    fetch(fullAPIURL)
+        .then(response => response.json())
+        .then(data => {
+            let view = ''
+            view += `<div class="weather__mainInfo">`
+            view += `<div class="weather__temp">
+            <span class="degree">${data.current.temp_c}</span>
+            <span class="unit">&deg; C</span>
+        </div>`
+            view += `<div class = "weather__icon">
+        <img src ="${data.current.condition.icon}" alt ="${data.current.condition.text}">
+        </div>`
+         view += `<div class = "weather__data">
+         <p>The amount of rainfall:<b> ${data.current.precip_mm} mm</b> </p>
+         <p>Humidity:<b> ${data.current.humidity} %</b> </p>
+         <p>Wind:<b> ${data.current.wind_kph} km/h</b> </p>
+         </div>`
     
+            view += `</div>`
+           // next days
+           view += `<h2 class= "weather__next">Next days weather:</h2>`
+           view += `<div class = "weather__days">`
+           // single day
+           data.forecast.forecastday.forEach(day => {
+               view += `<div class = "weather__day">`
+               view += `<div class = "weather__date">${day.date}</div>`
+               view += `<div class = "weather__icon">
+               <img src="${day.day.condition.icon}" alt="${day.day.condition.text}">
+               </div>`
+               view += `<div class = "weather__avg">${day.day.avgtemp_c}
+               &deg; C</div>`
+               view += `</div>`
+               // console.log(day)
+           })
+           view += `</div>`
+            weatherApi.innerHTML = view
+        })
 })
